2520134e911e0fb58b134489a5584de2dd5dc79b
Ömer Ayhan <om.ayhan247@gmail.com>
2021-09-03 14:13:26 +0300

    fixes and added tailwind

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,30 +1,30 @@
-import React,{useContext, useState} from 'react'
-import {QueryDataContext} from '../context/QueryContext'
+import React, { useContext, useState } from "react";
+import { QueryDataContext } from "../context/QueryContext";
 
 function SearchForm() {
-    const [query, setQuery] = useContext(QueryDataContext)
-    const [keyword, setKeyword]= useState('')
-    const handleSubmit = e =>{
-        e.preventDefault()
-        setQuery([keyword])
-        setKeyword('')
-    }
-    const handleChange = e =>{
-        setKeyword(e.target.value)
-    }
-    return (
-        <>
-        <form onSubmit = {handleSubmit}>
-            <input 
-            type="text"
-            placeholder="Enter Search Keyword"
-            value={keyword}
-            onChange={handleChange}
-            />
-            <button type="submit">Randomize</button>
-        </form>
-        </>
-    )
+  const [query, setQuery] = useContext(QueryDataContext);
+  const [keyword, setKeyword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setQuery([keyword]);
+    setKeyword("");
+  };
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter Search Keyword"
+          value={keyword}
+          onChange={handleChange}
+        />
+        <button type="submit">Randomize</button>
+      </form>
+    </>
+  );
 }
 
-export default SearchForm
+export default SearchForm;
--- src/context/QueryContext.js
+++ src/context/QueryContext.js
@@ -1,13 +1,14 @@
-import React,{createContext, useState} from 'react'
+import React, { createContext, useState } from "react";
 
-export const QueryDataContext =  createContext(); 
+export const QueryDataContext = createContext();
 
 function QueryProvider(props) {
-    const [query, setQuery] = useState('')
-    return (
-        <QueryDataContext.Provider value={[query, setQuery]}>
-            {props.children}
-        </QueryDataContext.Provider>
-    )
+  const [query, setQuery] = useState([]);
+  console.log(query);
+  return (
+    <QueryDataContext.Provider value={[query, setQuery]}>
+      {props.children}
+    </QueryDataContext.Provider>
+  );
 }
-export default QueryProvider
+export default QueryProvider;
